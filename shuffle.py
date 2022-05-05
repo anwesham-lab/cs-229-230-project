64ebb666c44de01e5bde09ece9e766ed29aeb735
anwesham-lab <anwesham@stanford.edu>
2022-05-05 16:17:10 -0700

    data processing i hate it here

--- shuffle.py
+++ shuffle.py
@@ -1,12 +1,21 @@
 import pandas as pd
-from pathlib import Path
+import numpy as np
 import argparse
 
 def shuffle(filename):
-    df = pd.read_csv(filename, header=None)
-    ds = df.sample(frac=1)
-    filename_new = filename[:-8] + 'shuffled.csv'
+    df = pd.read_csv(filename, header=None, names=['message', 'label'])
+    ds = df.sample(frac=1, random_state=30)
+    prefix = filename[:-7]
+    filename_new = prefix + 'shuffled.csv'
+    ds['message'] = ds['message'].str.replace(r'\"$', '')
+    ds['message'] = ds['message'].str.replace(r'^\"', '')
     ds.to_csv(filename_new, header=False, index=False)
+
+    train, dev, test = np.split(ds.sample(frac=1, random_state=42), [int(.8*len(df)), int(.9*len(df))])
+    train.to_csv(prefix + 'train.csv', header=False, index=False)
+    dev.to_csv(prefix + 'dev.csv', header=False, index=False)
+    test.to_csv(prefix + 'test.csv', header=False, index=False)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Add Arguments')
