f3c9058ec4ac8231ca632cf97dbb10797c81739a
anwesham-lab <anwesham@stanford.edu>
2022-05-05 20:12:19 -0700

    shuffle, preprocessing, and naive bayes

---
+++ naive_bayes.py
@@ -0,0 +1,244 @@
+import collections
+from cv2 import log
+import util
+import csv
+import numpy as np
+import argparse
+import re
+
+def load_sentiment_csv(path):
+    """Load the spam dataset from a TSV file
+
+    Args:
+         csv_path: Path to TSV file containing dataset.
+
+    Returns:
+        messages: A list of string devues containing the text of each message.
+        labels: The binary labels (0 or 1) for each message. A 1 indicates spam.
+    """
+
+    messages = []
+    labels = []
+
+    with open(path, 'r', newline='', encoding='utf8') as data_file:
+        reader = csv.reader(data_file, delimiter=',')
+
+        for label, message in reader:
+            messages.append(message[1:-1])
+            labels.append(1 if label == 'positive' else 0)
+
+    return messages, np.array(labels)
+
+def get_words(message):
+    """Get the normalized list of words from a message string.
+
+    This function should split a message into words, normalize them, and return
+    the resulting list. For splitting, you should split on spaces. For normalization,
+    you should convert everything to lowercase.
+
+    Args:
+        message: A string containing a sentiment message
+
+    Returns:
+       The list of normalized words from the message.
+    """
+
+    # *** START CODE HERE ***
+    message.replace('.', ' ')
+    message.replace(',', ' ')
+    message.replace(';', ' ')
+    message.replace(':', ' ')
+    message.replace('"', '')
+    message.replace('>', '')
+    message.replace('<', '')
+    message.replace('*', '')
+    message.replace('/', ' ')
+    tokens = message.split(" ")
+    return [token.lower() for token in tokens if token != ""]
+    # *** END CODE HERE ***
+
+
+def create_dictionary(messages):
+    """Create a dictionary mapping words to integer indices.
+
+    This function should create a dictionary of word to indices using the provided
+    training messages. Use get_words to process each message.
+
+    Rare words are often not useful for modeling. Please only add words to the dictionary
+    if they occur in at least five messages.
+
+    Args:
+        messages: A list of strings containing SMS messages
+
+    Returns:
+        A python dict mapping words to integers.
+    """
+
+    # *** START CODE HERE ***
+    counter = collections.Counter()
+    for message in messages:
+        print(set(get_words(message)))
+        counter.update(set(get_words(message)))
+    common_words = [word for word, count in counter.items() if count >= 5]
+    return {word: index for index, word in enumerate(common_words)}
+    # *** END CODE HERE ***
+
+
+def transform_text(messages, word_dictionary):
+    """Transform a list of text messages into a numpy array for further processing.
+
+    This function should create a numpy array that contains the number of times each word
+    of the vocabulary appears in each message. 
+    Each row in the resulting array should correspond to each message 
+    and each column should correspond to a word of the vocabulary.
+
+    Use the provided word dictionary to map words to column indices. Ignore words that
+    are not present in the dictionary. Use get_words to get the words for a message.
+
+    Args:
+        messages: A list of strings where each string is an SMS message.
+        word_dictionary: A python dict mapping words to integers.
+
+    Returns:
+        A numpy array marking the words present in each message.
+        Where the component (i,j) is the number of occurrences of the
+        j-th vocabulary word in the i-th message.
+    """
+    # *** START CODE HERE ***
+    count_arr = np.zeros((len(messages), len(word_dictionary)))
+    for index, message in enumerate(messages):
+        for word in get_words(message):
+            if word in word_dictionary:
+                count_arr[index, word_dictionary[word]] += 1
+    
+    return count_arr
+    # *** END CODE HERE ***
+
+
+def fit_naive_bayes_model(matrix, labels):
+    """Fit a naive bayes model.
+
+    This function should fit a Naive Bayes model given a training matrix and labels.
+
+    The function should return the state of that model.
+
+    Feel free to use whatever datatype you wish for the state of the model.
+
+    Args:
+        matrix: A numpy array containing word counts for the training data
+        labels: The binary (0 or 1) labels for that training data
+
+    Returns: The trained model
+    """
+
+    # *** START CODE HERE ***
+    
+    # calculate important priors + likelihood elements
+    class0 = matrix[labels == 0]
+    class1 = matrix[labels == 1]
+    phi = 1. * sum(labels) / len(labels)
+    theta0 = (class0).sum(axis=0) + 1
+    theta1 = (class1).sum(axis=0) + 1
+    theta0 /= theta0.sum()
+    theta1 /= theta1.sum()
+
+    # return np.log(phi0), np.log(phi1), np.log(theta0), np.log(theta1)
+    model = {}
+    model['log_phi0'] = np.log(1.-phi)
+    model['log_phi1'] = np.log(phi)
+    model['log_theta0'] = np.log(theta0)
+    model['log_theta1'] = np.log(theta1)
+
+    return model
+    # *** END CODE HERE ***
+
+
+def predict_from_naive_bayes_model(model, matrix):
+    """Use a Naive Bayes model to compute predictions for a target matrix.
+
+    This function should be able to predict on the models that fit_naive_bayes_model
+    outputs.
+
+    Args:
+        model: A trained model from fit_naive_bayes_model
+        matrix: A numpy array containing word counts
+
+    Returns: A numpy array containg the predictions from the model
+    """
+    # *** START CODE HERE ***
+    log_phi0 = model['log_phi0']
+    log_phi1 = model['log_phi1']
+    log_theta0 = model['log_theta0']
+    log_theta1 = model['log_theta1']
+    log_prob0 = log_phi0 + (matrix * log_theta0).sum(axis=1)
+    log_prob1 = log_phi1 + (matrix * log_theta1).sum(axis=1)
+    return ((log_prob1 > log_prob0).astype(int))
+    # *** END CODE HERE ***
+
+
+def get_top_five_naive_bayes_words(model, dictionary, positive):
+    """Compute the top five words that are most indicative of the spam (i.e positive) class.
+
+    Ues the metric given in part-c as a measure of how indicative a word is.
+    Return the words in sorted form, with the most indicative word first.
+
+    Args:
+        model: The Naive Bayes model returned from fit_naive_bayes_model
+        dictionary: A mapping of word to integer ids
+
+    Returns: A list of the top five most indicative words in sorted order with the most indicative first
+    """
+    # *** START CODE HERE ***
+    if positive:
+        top_indexes = np.argsort(model['log_theta0'] - model['log_theta1'])[:5]
+    else:
+        top_indexes = np.argsort(model['log_theta1'] - model['log_theta0'])[:5]
+    index_mappings = {index: word for word, index in dictionary.items()}
+    return [index_mappings[idx] for idx in top_indexes]
+    # *** END CODE HERE ***
+
+
+def main(train_path, dev_path, test_path, dict, matrix_save):
+    train_messages, train_labels = load_sentiment_csv(train_path)
+    dev_messages, dev_labels = load_sentiment_csv(dev_path)
+    test_messages, test_labels = load_sentiment_csv(test_path)
+
+    dictionary = create_dictionary(train_messages)
+    print('Size of dictionary: ', len(dictionary))
+    util.write_json(dict, dictionary)
+
+    train_matrix = transform_text(train_messages, dictionary)
+    np.savetxt(matrix_save, train_matrix[:100,:])
+
+    dev_matrix = transform_text(dev_messages, dictionary)
+    test_matrix = transform_text(test_messages, dictionary)
+
+    # naive_bayes_model = fit_naive_bayes_model(train_matrix, train_labels)
+    # naive_bayes_predictions = predict_from_naive_bayes_model(naive_bayes_model, test_matrix)
+    # np.savetxt('spam_naive_bayes_predictions', naive_bayes_predictions)
+
+    # naive_bayes_accuracy = np.mean(naive_bayes_predictions == test_labels)
+    # print('Naive Bayes had an accuracy of {} on the testing set'.format(naive_bayes_accuracy))
+
+    # top_5_pos_words = get_top_five_naive_bayes_words(naive_bayes_model, dictionary, True)
+    # print('The top 5 positive indicative words for Naive Bayes are: ', top_5_pos_words)
+    # util.write_json('positive_sentiment_top_indicative_words', top_5_pos_words)
+
+    # top_5_neg_words = get_top_five_naive_bayes_words(naive_bayes_model, dictionary, False)
+    # print('The top 5 positive indicative words for Naive Bayes are: ', top_5_neg_words)
+    # util.write_json('positive_sentiment_top_indicative_words', top_5_neg_words)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Add Arguments')
+    parser.add_argument('--train', default='imdb_data_train.csv', metavar='path', 
+                        required=False, help='training data path')
+    parser.add_argument('--dev', default='imdb_data_dev.csv', metavar='path', 
+                        required=False, help='dev/validation data path')
+    parser.add_argument('--test', default='imdb_data_test.csv', metavar='path', 
+                        required=False, help='testing data path')
+    parser.add_argument('--dict', default='imdb_dictionary', metavar='string', 
+                        required=False, help='Name of Dictionary file')
+    parser.add_argument('--matrix', default='imdb_sentiment_sample_matrix', metavar='string', 
+                        required=False, help='Name of Sample Matrix file')
+    args = parser.parse_args()
+    main(train_path=args.train, dev_path=args.dev, test_path=args.test, dict=args.dict, matrix_save=args.matrix)
---
+++ util.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+def add_intercept_fn(x):
+    """Add intercept to matrix x.
+
+    Args:
+        x: 2D NumPy array.
+
+    Returns:
+        New matrix same as x with 1's in the 0th column.
+    """
+    new_x = np.zeros((x.shape[0], x.shape[1] + 1), dtype=x.dtype)
+    new_x[:, 0] = 1
+    new_x[:, 1:] = x
+
+    return new_x
+
+def plot(x, y, theta, save_path, correction=1.0):
+    """Plot dataset and fitted logistic regression parameters.
+
+    Args:
+        x: Matrix of training examples, one per row.
+        y: Vector of labels in {0, 1}.
+        theta: Vector of parameters for logistic regression model.
+        save_path: Path to save the plot.
+        correction: Correction factor to apply (Problem 2(e) only).
+    """
+    # Plot dataset
+    plt.figure()
+    plt.plot(x[y == 1, -2], x[y == 1, -1], 'bx', linewidth=2)
+    plt.plot(x[y == 0, -2], x[y == 0, -1], 'go', linewidth=2)
+
+    # Plot decision boundary (found by solving for theta^T x = 0)
+    x1 = np.arange(min(x[:, -2]), max(x[:, -2]), 0.01)
+    x2 = -(theta[0] / theta[2] * correction + theta[1] / theta[2] * x1)
+    plt.plot(x1, x2, c='red', linewidth=2)
+
+    # Add labels and save to disk
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.savefig(save_path)
+
+
+def plot_contour(predict_fn):
+    """Plot a contour given the provided prediction function"""
+    x, y = np.meshgrid(np.linspace(-10, 10, num=20), np.linspace(-10, 10, num=20))
+    z = np.zeros(x.shape)
+
+    for i in range(x.shape[0]):
+        for j in range(y.shape[1]):
+            z[i, j] = predict_fn([x[i, j], y[i, j]])
+
+    plt.contourf(x, y, z, levels=[-float('inf'), 0, float('inf')], colors=['orange', 'cyan'])
+
+def plot_points(x, y):
+    """Plot some points where x are the coordinates and y is the label"""
+    x_one = x[y == 0, :]
+    x_two = x[y == 1, :]
+
+    plt.scatter(x_one[:,0], x_one[:,1], marker='x', color='red')
+    plt.scatter(x_two[:,0], x_two[:,1], marker='o', color='blue')
+
+def write_json(filename, value):
+    """Write the provided value as JSON to the given filename"""
+    with open(filename, 'w') as f:
+        json.dump(value, f)
