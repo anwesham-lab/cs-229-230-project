99b6065fe2edde98ea39c9b493e4b1b282411f3e
olivialeeyy <oliviayl@stanford.edu>
2022-06-04 03:08:47 -0700

    Update log_reg_small.py

--- log_reg_small.py
+++ log_reg_small.py
@@ -10,23 +10,38 @@ import util
 import matplotlib
 from sklearn.linear_model import LogisticRegression
 
-def log_reg(X_train, y_train, X_test, y_test, min_freq):
+def log_reg(X_train, y_train, X_valid, y_valid, X_test, y_test, min_freq, step_size=0.01, max_iter=1000000, eps=1e-5):
 	print("Started")
-	
 	
 	sc = StandardScaler()
 	X_train = sc.fit_transform(X_train)
 	X_test = sc.transform(X_test)
-	print("Scaled")
+	print("Scaled train")
 	
+	theta = np.zeros(X_train.shape[1])
+	valid_acc = None
+	for i in range(max_iter):
+		preds_val = 1 / (1 + np.exp(-np.dot(X_valid, theta)))
+		valid_acc_new = metrics.accuracy_score(y_valid, preds_val)
+		if valid_acc != None and valid_acc_new - valid_acc < 0:
+			step_size *= 0.9
+		elif valid_acc_new - valid_acc < eps:
+			break
+		valid_acc = valid_acc_new
 
-	print(X_train)
-	print(y_train)
-	clf_lr = LogisticRegression(random_state=0, class_weight = 'balanced')
-	clf_lr.fit(X_train, y_train)
+		orig_theta = theta.copy()
+		g_thetax = 1 / (1 + np.exp(-np.dot(X_train, theta)))
+		update = np.dot(X_train.T, g_thetax - y_train) / X_train.shape[0]
+		theta -= step_size * update
+		if np.linalg.norm(theta - orig_theta) < eps:
+			break
+
+	#clf_lr = LogisticRegression(random_state=0, class_weight = 'balanced')
+	#clf_lr.fit(X_train, y_train)
 	print("Fit")
 	# print('Logistic Regression Train Accuracy: ', clf_lr.score(X_train, y_train))
-	predictions = clf_lr.predict(X_test)
+	predictions = 1 / (1 + np.exp(-np.dot(X_test, theta)))
+	#predictions = clf_lr.predict(X_test)
 	print("Predict")
 	accuracy = metrics.accuracy_score(y_test, predictions)
 	f1_score = metrics.f1_score(y_test, predictions)
@@ -51,7 +66,7 @@ def main():
 		valid_matrix = transform_text(valid_reviews, dictionary)
 		test_matrix = transform_text(test_reviews, dictionary)
 		print("Transformed")
-		log_reg(train_matrix, train_labels, test_matrix, test_labels, freq)
+		log_reg(train_matrix, train_labels, valid_matrix, valid_labels, test_matrix, test_labels, freq)
 
 
 if __name__ == "__main__":
